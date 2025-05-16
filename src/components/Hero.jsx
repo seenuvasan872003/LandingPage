235770346a748b14529460f37dd99d777900f305
My Name <seenuifo.2003@gmail.com>
2025-05-16 22:15:20 +0530

    change the hero section title coloe change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,18 @@ import AnimationContainer from './AnimationContainer.jsx';
 const Hero = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex-grow grid md:grid-cols-2 grid-cols-1 bg-black py-20">
+    <div className="flex-grow grid md:grid-cols-2 grid-cols-1 bg-black py-20 justify-center items-center">
       {/* Left Column - Text Content */}
-      <div className="flex flex-col justify-center px-6 md:px-10 lg:px-16 py-16 bg-black">
+      <div className="flex flex-col justify-center px-6 md:px-10 lg:px-16 py-16 bg-black text-center md:text-left">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white leading-tight mb-6">
-          Automate your
-          <br />
-          business with
-          <br />
-          WhatsApp API!
+          <span className=' md:block inline'>Automate your</span>
+          <span className=' md:block inline'>business with</span>
+          <span className='bg-gradient-to-r from-green-400 to-green-600 bg-clip-text text-transparent inline md:block'>WhatsApp API!</span>
         </h1>
-        <p className="text-lg text-gray-400 mb-10 max-w-lg">
+        <p className="text-lg text-gray-400 mb-10 max-w-lg text-center md:text-left">
           Engage customers, streamline support, and boost sales with our powerful WhatsApp Business API integration platform.
         </p>
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 justify-center md:justify-start">
           <button className="px-6 py-2 border border-white text-white rounded-full hover:bg-white hover:text-black transition z-10" onClick={() => navigate('/book-demo')}>
             Book a demo
           </button>
