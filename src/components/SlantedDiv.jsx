39dbc2bea8b880f58aa7334a5e64e3e0bc302877
My Name <seenuifo.2003@gmail.com>
2025-05-16 22:22:23 +0530

    fit the image to origlal fix pixal

--- src/components/SlantedDiv.jsx
+++ src/components/SlantedDiv.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './SlantedDiv.css';
 
 const SlantedDiv = ({ children }) => {
-  return <div className="slanted-div">{children}</div>;
+  return <div className="slanted-div p-4">{children}</div>;
 };
 
 export default SlantedDiv;
