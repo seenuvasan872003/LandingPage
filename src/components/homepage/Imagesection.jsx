ffbfd6933a2731ee021345c9eca22a95af9e992f
My Name <seenuifo.2003@gmail.com>
2025-06-21 13:52:40 +0530

    fix the image section

--- src/components/homepage/Imagesection.jsx
+++ src/components/homepage/Imagesection.jsx
@@ -1,13 +1,19 @@
 import SlantedDiv from './SlantedDiv';
+import dashbordimg from '../../assets/dashbordimg.png';
 
 const Imagesection = ({ theme}) => {
   return (
     <SlantedDiv theme={theme} className={`${theme === 'light' ? 'bg-white' : 'bg-black'}`}>
-        <img 
+        {/* <img 
             src="https://i.ibb.co/sdRcKzX1/c872f192-f36f-4038-98ac-13708e49957d.jpg"
             alt="Image preview"
             className=" rounded" 
-          />
+          /> */}
+        <img 
+            src={dashbordimg}
+            alt="Dashboard"
+            className="rounded"
+        />   
     </SlantedDiv>
   )
 }
